f1d5c035368c625e92b5c25a43f77d16c4c7643e
jamesdymagiba <150773927+jamesdymagiba@users.noreply.github.com>
2024-09-24 23:30:30 +0800

    disabled swiping to open menu drawer on edit profile

--- app/src/main/java/com/example/wejeep/EPPassenger.java
+++ app/src/main/java/com/example/wejeep/EPPassenger.java
@@ -1,13 +1,13 @@
 package com.example.wejeep;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-
+import androidx.drawerlayout.widget.DrawerLayout;
 public class EPPassenger extends AppCompatActivity {
     Button btnBackEPP;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +15,13 @@ public class EPPassenger extends AppCompatActivity {
         setContentView(R.layout.activity_eppassenger);
 
         btnBackEPP = findViewById(R.id.btnBackEPP);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        // Disable swipe to open the drawer
+        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
 
         btnBackEPP.setOnClickListener(view -> {
             startActivity(new Intent(EPPassenger.this, PPassenger.class));
         });
-
-
     }
 }
