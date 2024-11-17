46b8050623332651d25f8ed74fa65f54c8fb2018
GOnlyG <zausazausa54@gmail.com>
2024-11-18 01:02:58 +0800

    fixed schedule menu

--- app/src/main/java/com/example/wejeep/Schedule.java
+++ app/src/main/java/com/example/wejeep/Schedule.java
@@ -1,5 +1,6 @@
 package com.example.wejeep;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -16,6 +17,7 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -51,27 +53,31 @@ public class Schedule extends AppCompatActivity {
         // Initialize the toolbar
         Toolbar toolbar = findViewById(R.id.toolbarSchedule);
 
+        // Initialize navigationManager and navigationView for menuVisibilityManager
+        navigationManager = new NavigationManager(this);
+        NavigationView navigationView = findViewById(R.id.nav_view);
 
-        // Initialize DrawerLayout and NavigationView
+        // Initialize MenuVisibilityManager with the NavigationView
+        Menu menu = navigationView.getMenu();
+        menuVisibilityManager = new MenuVisibilityManager(this);
+        menuVisibilityManager.fetchUserRole(menu);
+
+        //Initialize Drawer for menu
         drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_view);
-
-        // Set up DrawerToggle for Navigation Drawer
         ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
         drawerLayout.addDrawerListener(drawerToggle);
         drawerToggle.syncState();
 
-        // Set up NavigationManager and MenuVisibilityManager
-        navigationManager = new NavigationManager(this);
-        menuVisibilityManager = new MenuVisibilityManager(this);
-        navigationView.setNavigationItemSelectedListener(item -> {
-            boolean handled = navigationManager.handleNavigationItemSelected(item, Schedule.this);
-            drawerLayout.closeDrawer(GravityCompat.START);
-            return handled;
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                boolean handled = navigationManager.handleNavigationItemSelected(item, Schedule.this);
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return handled;
+            }
         });
 
-        // Check user authentication and set the profile info
-        checkUserAuthentication();
+        UserProfileManager.checkAuthAndUpdateUI(FirebaseAuth.getInstance(), navigationView, this);
 
         // Retrieve schedule data from Firestore
         fetchScheduleData();
@@ -104,34 +110,6 @@ public class Schedule extends AppCompatActivity {
                     Log.e(TAG, "Error fetching schedule data", e);
                 });
     }
-
-    private void checkUserAuthentication() {
-        FirebaseUser user = auth.getCurrentUser();
-
-        if (user == null) {
-            // User is not logged in, redirect to Login activity
-            Intent intent = new Intent(getApplicationContext(), Login.class);
-            startActivity(intent);
-            finish();
-        } else {
-            // User is logged in, update UI with user information
-            View headerView = navigationView.getHeaderView(0);
-            ImageView ivProfilePictureHSP = headerView.findViewById(R.id.ivProfilePictureHSP);
-            TextView tvNameHSP = headerView.findViewById(R.id.tvNameHSP);
-
-            tvNameHSP.setText(user.getDisplayName());
-            if (user.getPhotoUrl() != null) {
-                Glide.with(this)
-                        .load(user.getPhotoUrl())
-                        .apply(RequestOptions.circleCropTransform())
-                        .into(ivProfilePictureHSP);
-            }
-
-            // Fetch the user's role to update menu visibility
-            menuVisibilityManager.fetchUserRole(navigationView.getMenu());
-        }
-    }
-
     @Override
     public void onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
